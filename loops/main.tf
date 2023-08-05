ac1cc1fcf5381ecb721b9e41a2e743ed9dcefca8
Cbalajireddy <balaji22885@gmail.com>
2023-08-05 17:22:02 +0530

    abc

---
+++ loops/main.tf
@@ -0,0 +1,27 @@
+ariable "ami"{
+    default = "ami-03265a0778a880afb"
+}
+variable "instance_type"{
+    default = "t3.micro"
+}
+
+variable "security_group"{
+    default = ["sg-0490a4409ed09ba21"]
+}
+
+variable "components_dict"{
+    "frontend" = {},
+    "mongo" ={},
+    "catalogue" ={}
+}
+
+resource "aws_instance" instance123 {
+    for_each = var.components_dict
+    ami = var.ami
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group
+
+    tags = {
+        Name = each.key
+    }
+}
